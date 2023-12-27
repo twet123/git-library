bb2e07c484ee35612aee4b6ecd714841f1cd76b8
Vladimir Popov <vladapopov02@gmail.com>
2023-12-28 00:10:20 +0100

    feat: add Tree model

--- lib/src/main/kotlin/git/lib/domain/Tree.kt
+++ lib/src/main/kotlin/git/lib/domain/Tree.kt
@@ -1,4 +1,12 @@
 package git.lib.domain
 
+import git.lib.util.HashUtils
+
 class Tree {
+    private val entries: HashMap<String, Any> = HashMap()
+    val hash = HashUtils.calculateSHA1(entries.toString())
+
+    fun addEntry(name: String, entry: Any) {
+        entries[name] = entry
+    }
 }
