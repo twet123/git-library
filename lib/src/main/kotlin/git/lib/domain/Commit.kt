20e6670d8a9b49f6b8f7bd1d1591d31120968305
Vladimir Popov <vladapopov02@gmail.com>
2023-12-28 00:10:30 +0100

    feat: add Tree model

--- lib/src/main/kotlin/git/lib/domain/Commit.kt
+++ lib/src/main/kotlin/git/lib/domain/Commit.kt
@@ -1,4 +1,8 @@
 package git.lib.domain
 
-class Commit {
+import git.lib.util.HashUtils
+
+class Commit(val tree: Tree, val author: String, val message: String) {
+    val timestamp = System.currentTimeMillis()
+    val hash = HashUtils.calculateSHA1(tree.hash + author + message + timestamp)
 }
