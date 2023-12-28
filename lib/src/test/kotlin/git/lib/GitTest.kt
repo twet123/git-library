135638a62625584294be0a0a7154ecaa14a3d2e5
Vladimir Popov <vladapopov02@gmail.com>
2023-12-28 19:51:58 +0100

    feat: finish adding tests

--- lib/src/test/kotlin/git/lib/GitTest.kt
+++ lib/src/test/kotlin/git/lib/GitTest.kt
@@ -5,7 +5,6 @@ package git.lib
 
 import git.lib.domain.Blob
 import git.lib.domain.Tree
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
 import kotlin.test.*
 
@@ -42,7 +41,6 @@ class GitTest {
     fun testFindCommitByHash() {
         val git = Git()
         val tree = Tree()
-
         git.createCommit(tree, author, message)
 
         val commitHash = git.listCommits()[0].hash
@@ -56,7 +54,6 @@ class GitTest {
     fun testFindCommitByAuthor() {
         val git = Git()
         val tree = Tree()
-
         git.createCommit(tree, author, message)
         git.createCommit(tree, author + 1, message + 1)
         git.createCommit(tree, author, message + 2)
@@ -72,21 +69,71 @@ class GitTest {
 
     @Test
     fun testFindCommitsByMessage() {
+        val git = Git()
+        val tree = Tree()
+        git.createCommit(tree, author, "Fix: $message")
+        git.createCommit(tree, author, "Fix: $message 1")
+        git.createCommit(tree, author, message)
 
+        val foundCommits = git.findCommitsByMessage("Fix")
+
+        assertEquals(foundCommits.size, 2)
+        foundCommits.forEach { commit -> commit.message.contains("Fix")}
     }
 
     @Test
     fun testFindCommitsBeforeTimestamp() {
+        val git = Git()
+        val tree = Tree()
+        git.createCommit(tree, author, message)
+        git.createCommit(tree, author, message)
+        val timestamp = System.currentTimeMillis()
+        git.createCommit(tree, author, message)
 
+        val foundCommits = git.findCommitsBeforeTimestamp(timestamp)
+
+        assertEquals(foundCommits.size, 2)
+        foundCommits.forEach { commit -> assertTrue { commit.timestamp < timestamp } }
     }
 
     @Test
     fun testFindCommitsAfterTimestamp() {
+        val git = Git()
+        val tree = Tree()
+        git.createCommit(tree, author, message)
+        git.createCommit(tree, author, message)
+        val timestamp = System.currentTimeMillis()
+        git.createCommit(tree, author, message)
 
+        val foundCommits = git.findCommitsAfterTimestamp(timestamp)
+
+        assertEquals(foundCommits.size, 1)
+        foundCommits.forEach { commit -> assertTrue { commit.timestamp > timestamp } }
     }
 
     @Test
     fun testRedundantData() {
+        val git = Git()
+        val workTree = Tree()
+        val blob1 = Blob("sample data1")
+        val blob2 = Blob("sample data2")
+        val blob3 = Blob("sample data3")
+        val tree1 = Tree()
+        val tree2 = Tree()
+        tree1.addEntry("blob1", blob1)
+        tree2.addEntry("tree1", tree1)
+        tree2.addEntry("blob2", blob2)
+        workTree.addEntry("tree2", tree2)
+        workTree.addEntry("blob3", blob3)
+        git.createCommit(workTree, author, message)
 
+        val workTree1 = Tree()
+        val blob11 = Blob("sample data1")
+        val blob21 = Blob("sample data2")
+        workTree1.addEntry("blob11", blob11)
+        workTree1.addEntry("blob21", blob21)
+        git.createCommit(workTree1, author, message)
+
+        assertEquals(git.getBlobEntriesSize(), 3)
     }
 }
