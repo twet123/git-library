87530c4d13b8505d59f2f252467f825b0ab7ebe1
Vladimir Popov <vladapopov02@gmail.com>
2024-01-10 01:32:20 +0100

    feat: add parent to commit

--- lib/src/main/kotlin/git/lib/domain/Commit.kt
+++ lib/src/main/kotlin/git/lib/domain/Commit.kt
@@ -2,7 +2,7 @@ package git.lib.domain
 
 import git.lib.util.HashUtils
 
-class Commit(val tree: Tree, val author: String, val message: String) {
+class Commit(val tree: Tree, val author: String, val message: String, val parent: Commit?) {
     val timestamp = System.currentTimeMillis()
     val hash = HashUtils.calculateSHA1(tree.hash + author + message + timestamp)
 }
